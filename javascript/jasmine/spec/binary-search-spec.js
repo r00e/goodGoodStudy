30864b991c60277fcda6f4688a1923e810d8a759
r00e <ryang@thoughtworks.com>
2013-10-01 22:43:20 +0800

    [rui] 3rd way, I just want the recursive way could be seen more pure...

--- javascript/jasmine/spec/binary-search-spec.js
+++ javascript/jasmine/spec/binary-search-spec.js
@@ -50,3 +50,29 @@ describe("chopIteration", function()
 	});
 });
 
+describe("chopFunctionCall", function() 
+	{
+
+	it("should return -1 when array is empty", function(){
+		var array = [];
+		expect(chopFunctionCall(3, array)).toEqual(-1);
+	});
+
+	it("should return -1 when target is NOT in array", function(){
+		var array = [1];
+		expect(chopFunctionCall(3, array)).toEqual(-1);
+	});
+
+	it("should return 0 when target is the 1st element in array", function(){
+		var array = [1];
+		expect(chopFunctionCall(1, array)).toEqual(0);
+	});
+
+	it("should return right position when target is in array", function(){
+		var array = [1,3,5];
+		expect(chopFunctionCall(1, array)).toEqual(0);
+		expect(chopFunctionCall(3, array)).toEqual(1);
+		expect(chopFunctionCall(5, array)).toEqual(2);
+	});
+});
+
--- javascript/jasmine/src/binary-search.js
+++ javascript/jasmine/src/binary-search.js
@@ -1,3 +1,4 @@
+// recursive style
 function chopRecursion(target, array) {
 
 	var midPoint = parseInt(array.length/2);
@@ -32,8 +33,7 @@ function getResult(mid, position){
     }
 }
 
-
-
+// iteration style
 function chopIteration(target, array){
     var left = 0;
     var mid = parseInt(array.length/2);
@@ -55,4 +55,33 @@ function chopIteration(target, array){
         mid = parseInt((left + right)/2);
     }
     return -1;
+}
+
+// function call style
+function chopFunctionCall(target, array){
+    return binarySearch(target, array, 0, array.length);
+}
+
+function binarySearch(target, array, left, right){
+    var mid = parseInt((left + right)/2);
+
+    if(left < right)
+    {
+        if(target > array[mid])
+        {
+            return binarySearch(target, array, mid+1, right);
+        }
+        else if(target < array[mid])
+        {
+            return binarySearch(target, array, left, mid);
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    else
+    {
+        return -1;
+    }
 }
