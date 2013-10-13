b653526af0014429ec427826527bc2c4fe084184
r00e <ryang@thoughtworks.com>
2013-10-13 16:19:09 +0800

    restructure findInHalf

--- javascript/src/binary-search-new.js
+++ javascript/src/binary-search-new.js
@@ -3,22 +3,17 @@ function chopRecursion(target, array) {
 }
 
 function findInHalf(target, array, start, end){
-    if(start > end){
-        return -1;
-    }
-    else if(end == start && target == array[start]){
-        return end;
-    }
-
     var mid = parseInt( (start + end) / 2 );
-    if( target == array[mid] ){
-        return mid;
-    }
-    else if(target < array[mid]){
-        return findInHalf(target, array, start, mid-1);
-    }
-    else if(target > array[mid]){
-        return findInHalf(target, array, mid+1, end);
+    if(start < end){
+        if(target < array[mid]){
+            return findInHalf(target, array, start, mid);
+        }
+        else if(target > array[mid]){
+            return findInHalf(target, array, mid+1, end);
+        }
+        else{
+            return mid;
+        }
     }
     else
         return -1;
