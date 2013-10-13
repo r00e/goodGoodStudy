f8408fce00c2acdb1dac2eadc9d8d066d33d1b2b
r00e <ryang@thoughtworks.com>
2013-10-13 14:44:02 +0800

    return correct postion when target is at the mid of array

--- javascript/src/binary-search-new.js
+++ javascript/src/binary-search-new.js
@@ -1,7 +1,14 @@
-// recursive style
 function chopRecursion(target, array) {
-    if(array.length == 1 && target == array[0]){
+    if(array.length == 0){
+        return -1;
+    }
+    else if(array.length == 1 && target == array[0]){
         return 0;
+    }
+
+    var mid = parseInt(array.length/2);
+    if( target == array[mid] ){
+        return mid;
     }
     else
         return -1;
--- javascript/test/binary-search-new/binary-search-spec-new.js
+++ javascript/test/binary-search-new/binary-search-spec-new.js
@@ -1,5 +1,4 @@
-describe("chopRecursion", function() 
-	{
+describe("chopRecursion", function(){
 
 	it("should return -1 when array is empty", function(){
 		var array = [];
@@ -9,5 +8,13 @@ describe("chopRecursion", function()
     it("should return 0 when array only has 1 element and target is got", function(){
         var array = [3];
 		expect(chopRecursion(3, array)).toEqual(0);
+    });
+
+    it("should return correct postion when the target is at the middle of array", function(){
+        var array = [1,3];
+		expect(chopRecursion(3, array)).toEqual(1);
+
+        array = [2,4,6];
+		expect(chopRecursion(4, array)).toEqual(1);
     });
 });
