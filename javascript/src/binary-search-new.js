a1446821d79d0a7b77ae76ee36b21d0628d3d933
r00e <ryang@thoughtworks.com>
2013-10-13 15:21:22 +0800

    return correct position when target is at the left half of mid

--- javascript/src/binary-search-new.js
+++ javascript/src/binary-search-new.js
@@ -10,6 +10,28 @@ function chopRecursion(target, array) {
     if( target == array[mid] ){
         return mid;
     }
+    else if(target < array[mid]){
+        return findInLeftHalf(target, array, mid-1);    
+    }
+    else
+        return -1;
+}
+
+function findInLeftHalf(target, array, end){
+    if(end < 0){
+        return -1;
+    }
+    else if(end == 0 && target == array[0]){
+        return end;
+    }
+
+    var mid = parseInt( end/2 );
+    if( target == array[mid] ){
+        return mid;
+    }
+    else if(target < array[mid]){
+        return findInLeftHalf(target, array, mid-1);
+    }
     else
         return -1;
 }
--- javascript/test/binary-search-new/binary-search-spec-new.js
+++ javascript/test/binary-search-new/binary-search-spec-new.js
@@ -10,11 +10,25 @@ describe("chopRecursion", function(){
 		expect(chopRecursion(3, array)).toEqual(0);
     });
 
-    it("should return correct postion when the target is at the middle of array", function(){
+    it("should return correct position when the target is at the middle of array", function(){
         var array = [1,3];
 		expect(chopRecursion(3, array)).toEqual(1);
 
         array = [2,4,6];
 		expect(chopRecursion(4, array)).toEqual(1);
+    });
+
+    it("should return correct position when target is at the left side of mid",function(){
+        var array = [1,3];
+		expect(chopRecursion(1, array)).toEqual(0);
+
+        array = [2,4,6];
+		expect(chopRecursion(2, array)).toEqual(0);
+
+        array = [2,4,6,8,10,12];
+		expect(chopRecursion(4, array)).toEqual(1);
+
+        array = [1,3,5,7,9,11,13,15,17,19,21,23,25];
+		expect(chopRecursion(5, array)).toEqual(2);
     });
 });
