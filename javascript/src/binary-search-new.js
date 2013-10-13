319cad5eb97cf73b7240e18eade5385d2e129461
r00e <ryang@thoughtworks.com>
2013-10-13 15:23:28 +0800

    remove duplicated code

--- javascript/src/binary-search-new.js
+++ javascript/src/binary-search-new.js
@@ -1,20 +1,5 @@
 function chopRecursion(target, array) {
-    if(array.length == 0){
-        return -1;
-    }
-    else if(array.length == 1 && target == array[0]){
-        return 0;
-    }
-
-    var mid = parseInt(array.length/2);
-    if( target == array[mid] ){
-        return mid;
-    }
-    else if(target < array[mid]){
-        return findInLeftHalf(target, array, mid-1);    
-    }
-    else
-        return -1;
+    return findInLeftHalf(target, array, array.length);
 }
 
 function findInLeftHalf(target, array, end){
