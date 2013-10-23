5edc7106aa0b845c64c0e2807d4754cb946c56ba
r00e <ryang@thoughtworks.com>
2013-10-23 09:44:32 +0800

    init Bitmap with default value 0 and given size

---
+++ ruby/src/bloom-filter.rb
@@ -0,0 +1,7 @@
+def initBitmap(mapSize)
+    array = Array.new
+    mapSize.times do
+        array.push 0
+    end
+    return array
+end
--- ruby/test/bloom-filter-test.rb
+++ ruby/test/bloom-filter-test.rb
@@ -2,6 +2,10 @@ require "test/unit"
 require '../src/bloom-filter.rb'
 
 class BloomFilterTest < Test::Unit::TestCase
-    def  
+    def test_initBitmap
+        assert_equal([0,0,0], initBitmap(3), "should return [0,0,0] when initialize Bitmap when size is 3")
+        assert_not_equal([1,0,0], initBitmap(3), "all default value in Bitmap should be 0")
+        assert_not_equal([0,1,0], initBitmap(3), "all default value in Bitmap should be 0")
+        assert_not_equal([0,0,1], initBitmap(3), "all default value in Bitmap should be 0")
     end
 end
