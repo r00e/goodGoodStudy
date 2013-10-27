95890f2018fdd2463f0a67273a4f372f92f3bc01
r00e <twer@r00e.local>
2013-10-27 18:19:20 +0800

    bloom filter

--- ruby/src/bloom-filter.rb
+++ ruby/src/bloom-filter.rb
@@ -1,10 +1,32 @@
+require 'digest/md5'
+
 def initBitmap(mapSize)
     array = Array.new
     mapSize.times do
         array.push 0
     end
+    return array
 end
 
-def BloomFilter()
-    File.new('/usr/share/dict/words', 'r')
+def computeHashValue(word, startIndex, endIndex, mapSize)
+    digest = Digest::MD5.hexdigest(word)
+    return digest[startIndex, endIndex].to_i(16) % mapSize
 end
+
+def search(word, map)
+    [1,2,3,4,5].each{ |i| return false if map[computeHashValue(word, i, i+5, map.size)] == 0 }
+    return true 
+end
+
+def initBloomFilter(mapSize)
+    bitmaps = initBitmap(mapSize)
+
+    File.new('../test/words_test', 'r').each_line do |word|
+       [1,2,3,4,5].each { |i| bitmaps[computeHashValue(word.chomp, i, i+5, mapSize)] = 1 } 
+    end    
+
+    return bitmaps
+end
+
+#map = initBloomFilter(50)
+#puts search("xxx", map)
--- ruby/test/bloom-filter-test.rb
+++ ruby/test/bloom-filter-test.rb
@@ -4,8 +4,17 @@ require '../src/bloom-filter.rb'
 class BloomFilterTest < Test::Unit::TestCase
     def test_initBitmap
         assert_equal([0,0,0], initBitmap(3), "should return [0,0,0] when initialize Bitmap when size is 3")
+    	assert_not_equal([0,0,0,0], initBitmap(3), "should be an array which size is 3")
         assert_not_equal([1,0,0], initBitmap(3), "all default value in Bitmap should be 0")
         assert_not_equal([0,1,0], initBitmap(3), "all default value in Bitmap should be 0")
         assert_not_equal([0,0,1], initBitmap(3), "all default value in Bitmap should be 0")
+    end
+
+    def test_search
+        map = initBloomFilter(100)
+        
+        assert_equal(true, search("a", map))
+        assert_equal(true, search("Aaron", map))
+        assert_equal(false, search("xxx", map))
     end
 end
